4b45dd6fb4f628548d9e715de45d4b2c5f6d0903
AlexandruCozlovschi <alex.cozlovschi@gmail.com>
2024-08-22 14:20:09 +0300

    Test

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://birosolutions.md/');
+define('HTTP_SERVER', 'https://birosolutions.store/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://birosolutions.md/');
+define('HTTPS_SERVER', 'https://birosolutions.store/');
 
 // DIR
-define('DIR_APPLICATION', '/home/u621097813/domains/birosolutions.md/public_html/catalog/');
-define('DIR_SYSTEM', '/home/u621097813/domains/birosolutions.md/public_html/system/');
-define('DIR_IMAGE', '/home/u621097813/domains/birosolutions.md/public_html/image/');
-define('DIR_STORAGE', '/home/u621097813/domains/birosolutions.md/storage/');
+define('DIR_APPLICATION', '/home/u621097813/domains/birosolutions.store/public_html/catalog/');
+define('DIR_SYSTEM', '/home/u621097813/domains/birosolutions.store/public_html/system/');
+define('DIR_IMAGE', '/home/u621097813/domains/birosolutions.store/public_html/image/');
+define('DIR_STORAGE', '/home/u621097813/domains/birosolutions.store/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
