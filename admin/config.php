4e1cb55d066c4e78f240b8738aa72a078ffa1902
Birosolutions Store <prasada.team@gmail.com>
2024-09-01 10:34:18 +0000

    server

---
+++ admin/config.php
@@ -0,0 +1,38 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://birosolutions.store/admin/');
+define('HTTP_CATALOG', 'https://birosolutions.store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://birosolutions.store/admin/');
+define('HTTPS_CATALOG', 'https://birosolutions.store/');
+
+// DIR
+define('DIR_APPLICATION', '/home/u621097813/domains/birosolutions.store/public_html/oc39/admin/');
+define('DIR_SYSTEM', '/home/u621097813/domains/birosolutions.store/public_html/oc39/system/');
+define('DIR_IMAGE', '/home/u621097813/domains/birosolutions.store/public_html/image/');
+define('DIR_STORAGE', '/home/u621097813/domains/birosolutions.store/storage/');
+define('DIR_CATALOG', '/home/u621097813/domains/birosolutions.store/public_html/oc39/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u621097813_storeBir');
+define('DB_PASSWORD', 'zWMfx53xNhDEDGM');
+define('DB_DATABASE', 'u621097813_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+
+
+
