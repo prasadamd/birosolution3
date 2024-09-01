f136a91f64b5abc36eccae3e700388a43b31ee98
Birosolutions Store <prasada.team@gmail.com>
2024-09-01 09:16:06 +0000

    server cahnges

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://birosolutions.md/admin/');
-define('HTTP_CATALOG', 'https://birosolutions.md/');
+define('HTTP_SERVER', 'https://birosolutions.store/oc39/admin/');
+define('HTTP_CATALOG', 'https://birosolutions.store/oc39/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://birosolutions.md/admin/');
-define('HTTPS_CATALOG', 'https://birosolutions.md/');
+define('HTTPS_SERVER', 'https://birosolutions.store/oc39/admin/');
+define('HTTPS_CATALOG', 'https://birosolutions.store/oc39/');
 
 // DIR
-define('DIR_APPLICATION', '/home/u621097813/domains/birosolutions.md/public_html/admin/');
-define('DIR_SYSTEM', '/home/u621097813/domains/birosolutions.md/public_html/system/');
-define('DIR_IMAGE', '/home/u621097813/domains/birosolutions.md/public_html/image/');
-define('DIR_STORAGE', '/home/u621097813/domains/birosolutions.md/storage/');
-define('DIR_CATALOG', '/home/u621097813/domains/birosolutions.md/public_html/catalog/');
+define('DIR_APPLICATION', '/home/u621097813/domains/birosolutions.store/public_html/oc39/admin/');
+define('DIR_SYSTEM', '/home/u621097813/domains/birosolutions.store/public_html/oc39/system/');
+define('DIR_IMAGE', '/home/u621097813/domains/birosolutions.store/public_html/image/');
+define('DIR_STORAGE', '/home/u621097813/domains/birosolutions.store/storage/');
+define('DIR_CATALOG', '/home/u621097813/domains/birosolutions.store/public_html/oc39/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,11 +26,10 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'u621097813_birosolutions');
-define('DB_PASSWORD', 'DBBiro@birosolutions!1');
-define('DB_DATABASE', 'u621097813_Birosolutions');
+define('DB_USERNAME', 'u621097813_storeBir');
+define('DB_PASSWORD', 'zWMfx53xNhDEDGM');
+define('DB_DATABASE', 'u621097813_store');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
-
 // OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
